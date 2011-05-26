35da0a48b804ca18e8701b9eab5398308fa2c532
mikestone <stonemj@gmail.com>
2011-05-26 08:19:22 -0700

    First dogfed commit!!! YAY! This will help make the tests more readable.

--- src/test/javascript/mock-ejax.js
+++ src/test/javascript/mock-ejax.js
@@ -43,3 +43,38 @@ MockEjax.fn.setCursor = function(x, y) {
 
 MockEjax.fn.file = function(filename) {
 };
+
+var prepared = false;
+var charToCode = {};
+var charToShifted = {};
+
+function prepareCodes() {
+    if (prepared) {
+        return;
+    }
+
+    prepared = true;
+
+    for (var key in keyboard.standard.codeToKey) {
+        charToCode[keyboard.standard.codeToKey[key]] = key;
+    }
+
+    for (var key in keyboard.standard.codeToShiftedKey) {
+        charToCode[keyboard.standard.codeToShiftedKey[key]] = key;
+        charToShifted[keyboard.standard.codeToShiftedKey[key]] = true;
+    }
+
+    for (var key in keyboard.standard.codeToToken) {
+        charToCode[Token.specialKeys[keyboard.standard.codeToToken[key]]] = key;
+    }
+}
+
+MockEjax.fn.fireKeyDowns = function(keys) {
+    prepareCodes();
+    var tokens = parseBinding(keys);
+
+    for (var i = 0; i < tokens.length; i++) {
+        var token = tokens[i];
+        this.onKeyDown({ keyCode: charToCode[token.key], ctrl: token.control, alt: token.meta, shift: charToShifted[token] });
+    }
+};
