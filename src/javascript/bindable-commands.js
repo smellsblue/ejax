bc84140842c85b6f8047912aa440e48b0e530a35
mikestone <stonemj@gmail.com>
2011-05-29 00:26:09 -0700

    Basic help information for all the available functions.

--- src/javascript/bindable-commands.js
+++ src/javascript/bindable-commands.js
@@ -7,6 +7,7 @@ Ejax.bindable = function(options) {
 
 Ejax.bindable({
     name: "ringBell",
+    description: "Ring the bell and cancel certain commands.",
     fn: function() {
         // TODO: This function should ring the bell if possible, and then
         // cancel some actions like creating a keyboard macro.
@@ -16,6 +17,7 @@ Ejax.bindable({
 
 Ejax.bindable({
     name: "exit",
+    description: "Exit the application.",
     fn: function() {
         this.io.exit();
     }
@@ -23,6 +25,7 @@ Ejax.bindable({
 
 Ejax.bindable({
     name: "executeCommand",
+    description: "Execute any bindable command dynamically.  A parameter is read and that parameter is run if it is a bindable command.  The parameter can be tab completed.",
     fn: function() {
         var self = this;
 
@@ -54,6 +57,7 @@ Ejax.bindable({
 
 Ejax.bindable({
     name: "shell",
+    description: "Create a new interactive shell buffer and switch to that buffer.",
     fn: function() {
         var io = this.io;
         var buffer = new Buffer(this.screen, { name: "*shell*", mode: shellMode, columns: this.screen.columns, rows: this.screen.rows - 1, parameterMode: true });
@@ -76,6 +80,7 @@ Ejax.bindable({
 
 Ejax.bindable({
     name: "sendShellCommand",
+    description: "Send the current command that is entered to the current shell buffer to the shell.  The command is appended to the output and then removed from the prompt.",
     fn: function() {
         var parameter = this.screen.currentWindow.buffer.content.getParameter();
 
@@ -91,6 +96,7 @@ Ejax.bindable({
 
 Ejax.bindable({
     name: "moveForward",
+    description: "Move forward by one character.",
     fn: function() {
         this.screen.currentWindow.buffer.moveForward();
     }
@@ -98,6 +104,7 @@ Ejax.bindable({
 
 Ejax.bindable({
     name: "moveBackward",
+    description: "Move backward by one character.",
     fn: function() {
         this.screen.currentWindow.buffer.moveBackward();
     }
@@ -105,6 +112,7 @@ Ejax.bindable({
 
 Ejax.bindable({
     name: "nextLine",
+    description: "Move down by one line.",
     fn: function() {
         this.screen.currentWindow.buffer.nextLine();
     }
@@ -112,6 +120,7 @@ Ejax.bindable({
 
 Ejax.bindable({
     name: "previousLine",
+    description: "Move up by one line.",
     fn: function() {
         this.screen.currentWindow.buffer.previousLine();
     }
@@ -119,6 +128,7 @@ Ejax.bindable({
 
 Ejax.bindable({
     name: "gotoLine",
+    description: "Go to a specific line, read as a parameter.  If the line is not currently visible, the buffer will be centered on that line.  If the line is less than the first line, it will be changed to the first line.  Likewise, if the line is greater than the last line, it will be changed to the last line.",
     fn: function() {
         var self = this;
 
@@ -140,6 +150,7 @@ Ejax.bindable({
 
 Ejax.bindable({
     name: "insertSelf",
+    description: "Insert the last typed key to the current buffer (if it is a printable key).",
     fn: function() {
         if (!Object.isNullOrUndefined(this.lastKey)) {
             this.screen.currentWindow.buffer.insert(this.lastKey);
@@ -149,6 +160,7 @@ Ejax.bindable({
 
 Ejax.bindable({
     name: "deleteForward",
+    description: "Delete one character at the cursor.",
     fn: function() {
         this.screen.currentWindow.buffer.deleteForward();
     }
@@ -156,6 +168,7 @@ Ejax.bindable({
 
 Ejax.bindable({
     name: "deleteBackward",
+    description: "Delete one character behind the cursor.",
     fn: function() {
         this.screen.currentWindow.buffer.deleteBackward();
     }
@@ -163,6 +176,7 @@ Ejax.bindable({
 
 Ejax.bindable({
     name: "mark",
+    description: "Mark the current cursor location.  The mark can then be used in a lot of region based functions like copyRegion.",
     fn: function() {
         this.screen.currentWindow.buffer.mark();
     }
@@ -170,6 +184,7 @@ Ejax.bindable({
 
 Ejax.bindable({
     name: "copyRegion",
+    description: "Copy from the last marked position to the current cursor position.  The copied region can then be 'yank'ed, which will paste it to the buffer.",
     fn: function() {
         this.screen.currentWindow.buffer.copyRegion();
     }
@@ -177,6 +192,7 @@ Ejax.bindable({
 
 Ejax.bindable({
     name: "killRegion",
+    description: "Kill from the last marked position to the current cursor position.  This will remove the region from the buffer and copy it so it can later be 'yank'ed.",
     fn: function() {
         this.screen.currentWindow.buffer.killRegion();
     }
@@ -184,6 +200,7 @@ Ejax.bindable({
 
 Ejax.bindable({
     name: "yank",
+    description: "Yank what was last copied or killed into the current buffer.",
     fn: function() {
         this.screen.currentWindow.buffer.yank();
     }
@@ -191,6 +208,7 @@ Ejax.bindable({
 
 Ejax.bindable({
     name: "lineStart",
+    description: "Move the cursor to the start of the current line.",
     fn: function() {
         this.screen.currentWindow.buffer.lineStart();
     }
@@ -198,6 +216,7 @@ Ejax.bindable({
 
 Ejax.bindable({
     name: "lineEnd",
+    description: "Move the cursor to the end of the current line.",
     fn: function() {
         this.screen.currentWindow.buffer.lineEnd();
     }
@@ -205,6 +224,7 @@ Ejax.bindable({
 
 Ejax.bindable({
     name: "bufferStart",
+    description: "Move the cursor to the beginning of the buffer.",
     fn: function() {
         this.screen.currentWindow.buffer.bufferStart();
     }
@@ -212,6 +232,7 @@ Ejax.bindable({
 
 Ejax.bindable({
     name: "bufferEnd",
+    description: "Move the cursor to the end of the buffer.",
     fn: function() {
         this.screen.currentWindow.buffer.bufferEnd(this.screen.currentWindow.rows - 1);
     }
@@ -219,6 +240,7 @@ Ejax.bindable({
 
 Ejax.bindable({
     name: "findFile",
+    description: "Find a file and open its contents into a new buffer, and show that buffer.  The filename is read as a parameter.  The filename can be tab completed.",
     fn: function() {
         var io = this.io;
         var screen = this.screen;
@@ -266,6 +288,7 @@ Ejax.bindable({
 
 Ejax.bindable({
     name: "saveBuffer",
+    description: "Save the current buffer.  Presumably, this buffer is a loaded file and so any changes will be written out to disk.",
     fn: function() {
         this.screen.currentWindow.buffer.saveBuffer();
     }
@@ -273,6 +296,7 @@ Ejax.bindable({
 
 Ejax.bindable({
     name: "changeBuffer",
+    description: "Change which buffer is being shown in the current window.  The buffer name is read as a parameter and can be tab completed.",
     fn: function() {
         var screen = this.screen;
         var nextName = screen.nextAvailableBuffer().name;
@@ -295,6 +319,7 @@ Ejax.bindable({
 
 Ejax.bindable({
     name: "killBuffer",
+    description: "Kill the current buffer.  Any changes not saved will be lost.  The most recent buffer shown will be loaded into the current window in its place.",
     fn: function() {
         var screen = this.screen;
         var name = screen.currentWindow.buffer.name;
@@ -317,6 +342,7 @@ Ejax.bindable({
 
 Ejax.bindable({
     name: "autoComplete",
+    description: "Auto complete the parameter being read.  The values for the auto complete depends entirely on the parameter being read.",
     fn: function() {
         // TODO: Support shell mode auto completing.
         if (this.screen.currentWindow.buffer.minibuffer) {
@@ -327,6 +353,7 @@ Ejax.bindable({
 
 Ejax.bindable({
     name: "parameterFinished",
+    description: "The parameter has finished being input, so the value is passed to whatever callback was registered for the parameter.  The function that requested the parameter is then completed.",
     fn: function() {
         this.screen.currentWindow = this.screen.minibuffer.status.lastWindow;
         var parameter = this.screen.minibuffer.content.getParameter();
@@ -341,6 +368,7 @@ Ejax.bindable({
 
 Ejax.bindable({
     name: "helpForFunction",
+    description: "Get help for a function.  The function name is read as a parameter and can be tab completed.",
     fn: function() {
         var self = this;
 
