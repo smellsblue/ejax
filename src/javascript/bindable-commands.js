c3a5be4d4b37e12101edb41548876a54ebd7a0b1
mikestone <stonemj@gmail.com>
2011-05-28 20:42:22 -0700

    Convert the arguments to a hash so it is more readable and we can add more options later.

--- src/javascript/bindable-commands.js
+++ src/javascript/bindable-commands.js
@@ -1,252 +1,337 @@
-Ejax.fn.ringBell = function() {
-    // TODO: This function should ring the bell if possible, and then
-    // cancel some actions like creating a keyboard macro.
-    this.io.beep();
+Ejax.bindable = function(options) {
+    Ejax.fn[options.name] = options.fn;
 };
 
-Ejax.fn.exit = function() {
-    this.io.exit();
-};
+Ejax.bindable({
+    name: "ringBell",
+    fn: function() {
+        // TODO: This function should ring the bell if possible, and then
+        // cancel some actions like creating a keyboard macro.
+        this.io.beep();
+    }
+});
 
-Ejax.fn.executeCommand = function() {
-    var self = this;
+Ejax.bindable({
+    name: "exit",
+    fn: function() {
+        this.io.exit();
+    }
+});
 
-    this.readParameter({
-        prompt: "M-x ",
-        callback: function(fnName) {
-            var fn = self[fnName];
+Ejax.bindable({
+    name: "executeCommand",
+    fn: function() {
+        var self = this;
 
-            if (fn && fn.isFunction()) {
-                self[fnName]();
-            } else {
-                self.screen.minibuffer.setBufferContent(fnName + " is undefined");
-            }
-        },
-        autoCompleteFn: function() {
-            var result = [];
+        this.readParameter({
+            prompt: "M-x ",
+            callback: function(fnName) {
+                var fn = self[fnName];
 
-            for (var property in Ejax.fn) {
-                if (Ejax.fn[property] && Ejax.fn[property].isFunction()) {
-                    result.push(property);
+                if (fn && fn.isFunction()) {
+                    self[fnName]();
+                } else {
+                    self.screen.minibuffer.setBufferContent(fnName + " is undefined");
                 }
+            },
+            autoCompleteFn: function() {
+                var result = [];
+
+                for (var property in Ejax.fn) {
+                    if (Ejax.fn[property] && Ejax.fn[property].isFunction()) {
+                        result.push(property);
+                    }
+                }
+
+                return result;
             }
-
-            return result;
-        }
-    });
-};
-
-Ejax.fn.shell = function() {
-    var io = this.io;
-    var buffer = new Buffer(this.screen, { name: "*shell*", mode: shellMode, columns: this.screen.columns, rows: this.screen.rows - 1, parameterMode: true });
-    buffer.shell = this.io.shell({
-        outputFn: function(str) {
-            io.addJob(function() {
-                buffer.append(str);
-                buffer.postRedraw();
-            });
-        }
-    });
-
-    if (!buffer.shell) {
-        throw new Error("Shell not supported!");
+        });
     }
+});
 
-    this.screen.addAndChangeBuffer(buffer);
-};
+Ejax.bindable({
+    name: "shell",
+    fn: function() {
+        var io = this.io;
+        var buffer = new Buffer(this.screen, { name: "*shell*", mode: shellMode, columns: this.screen.columns, rows: this.screen.rows - 1, parameterMode: true });
+        buffer.shell = this.io.shell({
+            outputFn: function(str) {
+                io.addJob(function() {
+                    buffer.append(str);
+                    buffer.postRedraw();
+                });
+            }
+        });
 
-Ejax.fn.sendShellCommand = function() {
-    var parameter = this.screen.currentWindow.buffer.content.getParameter();
+        if (!buffer.shell) {
+            throw new Error("Shell not supported!");
+        }
 
-    if (!parameter.endsWith("\n")) {
-            parameter += "\n";
+        this.screen.addAndChangeBuffer(buffer);
     }
+});
 
-    this.screen.currentWindow.buffer.content.setParameter("");
-    this.screen.currentWindow.buffer.append(parameter);
-    this.screen.currentWindow.buffer.shell.send(parameter);
-};
+Ejax.bindable({
+    name: "sendShellCommand",
+    fn: function() {
+        var parameter = this.screen.currentWindow.buffer.content.getParameter();
 
-Ejax.fn.moveForward = function() {
-    this.screen.currentWindow.buffer.moveForward();
-};
-
-Ejax.fn.moveBackward = function() {
-    this.screen.currentWindow.buffer.moveBackward();
-};
-
-Ejax.fn.nextLine = function() {
-    this.screen.currentWindow.buffer.nextLine();
-};
-
-Ejax.fn.previousLine = function() {
-    this.screen.currentWindow.buffer.previousLine();
-};
-
-Ejax.fn.gotoLine = function() {
-    var self = this;
-
-    this.readParameter({
-        prompt: "Goto line: ",
-        callback: function(line) {
-            if (!/^\d+$/.test(line)) {
-                self.screen.minibuffer.setBufferContent("Expected int, got " + line);
-                return;
-            }
-
-            line = parseInt(line, 10);
-            var window = self.screen.currentWindow;
-            window.buffer.gotoLine(line, window.rows - 1);
+        if (!parameter.endsWith("\n")) {
+                parameter += "\n";
         }
-    });
-};
 
-Ejax.fn.insertSelf = function() {
-    if (!Object.isNullOrUndefined(this.lastKey)) {
-        this.screen.currentWindow.buffer.insert(this.lastKey);
+        this.screen.currentWindow.buffer.content.setParameter("");
+        this.screen.currentWindow.buffer.append(parameter);
+        this.screen.currentWindow.buffer.shell.send(parameter);
     }
-};
+});
 
-Ejax.fn.deleteForward = function() {
-    this.screen.currentWindow.buffer.deleteForward();
-};
-
-Ejax.fn.deleteBackward = function() {
-    this.screen.currentWindow.buffer.deleteBackward();
-};
-
-Ejax.fn.mark = function() {
-    this.screen.currentWindow.buffer.mark();
-};
-
-Ejax.fn.copyRegion = function() {
-    this.screen.currentWindow.buffer.copyRegion();
-};
-
-Ejax.fn.killRegion = function() {
-    this.screen.currentWindow.buffer.killRegion();
-};
-
-Ejax.fn.yank = function() {
-    this.screen.currentWindow.buffer.yank();
-};
-
-Ejax.fn.lineStart = function() {
-    this.screen.currentWindow.buffer.lineStart();
-};
-
-Ejax.fn.lineEnd = function() {
-    this.screen.currentWindow.buffer.lineEnd();
-};
-
-Ejax.fn.bufferStart = function() {
-    this.screen.currentWindow.buffer.bufferStart();
-};
-
-Ejax.fn.bufferEnd = function() {
-    this.screen.currentWindow.buffer.bufferEnd(this.screen.currentWindow.rows - 1);
-};
-
-Ejax.fn.findFile = function() {
-    var io = this.io;
-    var screen = this.screen;
-
-    this.readParameter({
-        prompt: "Find file: ",
-        value: this.getWorkingDirectory(),
-        callback: function(filename) {
-            var file = io.file(filename);
-            var buffer = new Buffer(screen, { name: file.name(), file: file });
-            buffer.setBufferContent(file.contents());
-            screen.addAndChangeBuffer(buffer);
-        },
-        contextFn: function(value) {
-            var path = "";
-            var index = value.lastIndexOf(io.separator());
-
-            if (index >= 0) {
-                path = value.substring(0, index + 1);
-            }
-
-            return path;
-        },
-        contextAutoCompleteFn: function(path) {
-            var current;
-            var result = [];
-
-            if (path == "") {
-                current = io.file(".");
-            } else {
-                current = io.file(path);
-            }
-
-            var files = current.entries();
-
-            for (var i = 0; i < files.length; i++) {
-                result.push(path + files[i]);
-            }
-
-            return result;
-        }
-    });
-};
-
-Ejax.fn.saveBuffer = function() {
-    this.screen.currentWindow.buffer.saveBuffer();
-};
-
-Ejax.fn.changeBuffer = function() {
-    var screen = this.screen;
-    var nextName = screen.nextAvailableBuffer().name;
-
-    this.readParameter({
-        prompt: "Switch to buffer (default " + nextName + "): ",
-        callback: function(buffer) {
-            if (buffer == "") {
-                buffer = nextName;
-            }
-
-            screen.changeBuffer(screen.getOrCreateBuffer(buffer));
-        },
-        autoCompleteFn: function() {
-            return screen.getBufferNames();
-        }
-    });
-};
-
-Ejax.fn.killBuffer = function() {
-    var screen = this.screen;
-    var name = screen.currentWindow.buffer.name;
-
-    this.readParameter({
-        prompt: "Kill buffer (default " + name + "): ",
-        callback: function(buffer) {
-            if (buffer == "") {
-                buffer = name;
-            }
-
-            screen.killBuffer(screen.getBuffer(buffer));
-        },
-        autoCompleteFn: function() {
-            return screen.getBufferNames();
-        }
-    });
-};
-
-Ejax.fn.autoComplete = function() {
-    // TODO: Support shell mode auto completing.
-    if (this.screen.currentWindow.buffer.minibuffer) {
-        this.screen.currentWindow.buffer.status.autoComplete();
+Ejax.bindable({
+    name: "moveForward",
+    fn: function() {
+        this.screen.currentWindow.buffer.moveForward();
     }
-};
+});
 
-Ejax.fn.parameterFinished = function() {
-    this.screen.currentWindow = this.screen.minibuffer.status.lastWindow;
-    var parameter = this.screen.minibuffer.content.getParameter();
-    this.screen.minibuffer.content.set("");
-    var status = this.screen.minibuffer.status;
-    this.screen.minibuffer.status = null;
-    this.screen.minibufferWindow.postRedraw();
-    this.screen.resetCursor();
-    status.callback(parameter);
-};
+Ejax.bindable({
+    name: "moveBackward",
+    fn: function() {
+        this.screen.currentWindow.buffer.moveBackward();
+    }
+});
+
+Ejax.bindable({
+    name: "nextLine",
+    fn: function() {
+        this.screen.currentWindow.buffer.nextLine();
+    }
+});
+
+Ejax.bindable({
+    name: "previousLine",
+    fn: function() {
+        this.screen.currentWindow.buffer.previousLine();
+    }
+});
+
+Ejax.bindable({
+    name: "gotoLine",
+    fn: function() {
+        var self = this;
+
+        this.readParameter({
+            prompt: "Goto line: ",
+            callback: function(line) {
+                if (!/^\d+$/.test(line)) {
+                    self.screen.minibuffer.setBufferContent("Expected int, got " + line);
+                    return;
+                }
+
+                line = parseInt(line, 10);
+                var window = self.screen.currentWindow;
+                window.buffer.gotoLine(line, window.rows - 1);
+            }
+        });
+    }
+});
+
+Ejax.bindable({
+    name: "insertSelf",
+    fn: function() {
+        if (!Object.isNullOrUndefined(this.lastKey)) {
+            this.screen.currentWindow.buffer.insert(this.lastKey);
+        }
+    }
+});
+
+Ejax.bindable({
+    name: "deleteForward",
+    fn: function() {
+        this.screen.currentWindow.buffer.deleteForward();
+    }
+});
+
+Ejax.bindable({
+    name: "deleteBackward",
+    fn: function() {
+        this.screen.currentWindow.buffer.deleteBackward();
+    }
+});
+
+Ejax.bindable({
+    name: "mark",
+    fn: function() {
+        this.screen.currentWindow.buffer.mark();
+    }
+});
+
+Ejax.bindable({
+    name: "copyRegion",
+    fn: function() {
+        this.screen.currentWindow.buffer.copyRegion();
+    }
+});
+
+Ejax.bindable({
+    name: "killRegion",
+    fn: function() {
+        this.screen.currentWindow.buffer.killRegion();
+    }
+});
+
+Ejax.bindable({
+    name: "yank",
+    fn: function() {
+        this.screen.currentWindow.buffer.yank();
+    }
+});
+
+Ejax.bindable({
+    name: "lineStart",
+    fn: function() {
+        this.screen.currentWindow.buffer.lineStart();
+    }
+});
+
+Ejax.bindable({
+    name: "lineEnd",
+    fn: function() {
+        this.screen.currentWindow.buffer.lineEnd();
+    }
+});
+
+Ejax.bindable({
+    name: "bufferStart",
+    fn: function() {
+        this.screen.currentWindow.buffer.bufferStart();
+    }
+});
+
+Ejax.bindable({
+    name: "bufferEnd",
+    fn: function() {
+        this.screen.currentWindow.buffer.bufferEnd(this.screen.currentWindow.rows - 1);
+    }
+});
+
+Ejax.bindable({
+    name: "findFile",
+    fn: function() {
+        var io = this.io;
+        var screen = this.screen;
+
+        this.readParameter({
+            prompt: "Find file: ",
+            value: this.getWorkingDirectory(),
+            callback: function(filename) {
+                var file = io.file(filename);
+                var buffer = new Buffer(screen, { name: file.name(), file: file });
+                buffer.setBufferContent(file.contents());
+                screen.addAndChangeBuffer(buffer);
+            },
+            contextFn: function(value) {
+                var path = "";
+                var index = value.lastIndexOf(io.separator());
+
+                if (index >= 0) {
+                    path = value.substring(0, index + 1);
+                }
+
+                return path;
+            },
+            contextAutoCompleteFn: function(path) {
+                var current;
+                var result = [];
+
+                if (path == "") {
+                    current = io.file(".");
+                } else {
+                    current = io.file(path);
+                }
+
+                var files = current.entries();
+
+                for (var i = 0; i < files.length; i++) {
+                    result.push(path + files[i]);
+                }
+
+                return result;
+            }
+        });
+    }
+});
+
+Ejax.bindable({
+    name: "saveBuffer",
+    fn: function() {
+        this.screen.currentWindow.buffer.saveBuffer();
+    }
+});
+
+Ejax.bindable({
+    name: "changeBuffer",
+    fn: function() {
+        var screen = this.screen;
+        var nextName = screen.nextAvailableBuffer().name;
+
+        this.readParameter({
+            prompt: "Switch to buffer (default " + nextName + "): ",
+            callback: function(buffer) {
+                if (buffer == "") {
+                    buffer = nextName;
+                }
+
+                screen.changeBuffer(screen.getOrCreateBuffer(buffer));
+            },
+            autoCompleteFn: function() {
+                return screen.getBufferNames();
+            }
+        });
+    }
+});
+
+Ejax.bindable({
+    name: "killBuffer",
+    fn: function() {
+        var screen = this.screen;
+        var name = screen.currentWindow.buffer.name;
+
+        this.readParameter({
+            prompt: "Kill buffer (default " + name + "): ",
+            callback: function(buffer) {
+                if (buffer == "") {
+                    buffer = name;
+                }
+
+                screen.killBuffer(screen.getBuffer(buffer));
+            },
+            autoCompleteFn: function() {
+                return screen.getBufferNames();
+            }
+        });
+    }
+});
+
+Ejax.bindable({
+    name: "autoComplete",
+    fn: function() {
+        // TODO: Support shell mode auto completing.
+        if (this.screen.currentWindow.buffer.minibuffer) {
+            this.screen.currentWindow.buffer.status.autoComplete();
+        }
+    }
+});
+
+Ejax.bindable({
+    name: "parameterFinished",
+    fn: function() {
+        this.screen.currentWindow = this.screen.minibuffer.status.lastWindow;
+        var parameter = this.screen.minibuffer.content.getParameter();
+        this.screen.minibuffer.content.set("");
+        var status = this.screen.minibuffer.status;
+        this.screen.minibuffer.status = null;
+        this.screen.minibufferWindow.postRedraw();
+        this.screen.resetCursor();
+        status.callback(parameter);
+    }
+});
